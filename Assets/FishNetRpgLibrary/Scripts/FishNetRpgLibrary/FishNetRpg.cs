b2271570a0cdfe77fac639e5c9db1c5df4d6db6e
Brandon Anderson <brandon.anderson@amilious.com>
2022-06-30 02:26:07 +0800

    0.0.1.Statistics.007
    - Added FishNetRpg static class to hold constants.
    - Cleaned up the StatsModifier class.

---
+++ Assets/FishNetRpgLibrary/Scripts/FishNetRpgLibrary/FishNetRpg.cs
@@ -0,0 +1,11 @@
+namespace FishNetRpgLibrary {
+    
+    /// <summary>
+    /// This class is used to hold constants
+    /// </summary>
+    public static class FishNetRpg {
+
+        public const string ASSET_MENU_ROOT = "FishNetRpg/";
+
+    }
+}
--- Assets/FishNetRpgLibrary/Scripts/FishNetRpgLibrary/Statistics/StatModifier.cs
+++ Assets/FishNetRpgLibrary/Scripts/FishNetRpgLibrary/Statistics/StatModifier.cs
@@ -5,17 +5,23 @@ namespace FishNetRpgLibrary.Statistics {
     /// <summary>
     /// This struct is used to modify a stat.
     /// </summary>
+    [CreateAssetMenu(fileName = "NewStatModifier", 
+        menuName = FishNetRpg.ASSET_MENU_ROOT+"Stats/Stat Modifier", order = 0)]
     public class StatModifier : ScriptableObject, IStatModifier {
 
-        [SerializeField] private string stat;
-        [SerializeField] private ModifierOperation operation;
-        [SerializeField] private float amount;
-        [SerializeField] private float duration = -1;
+        [SerializeField, Tooltip("The name of the stat that the modifier will be applied to.")] 
+        private string statName;
+        [SerializeField, Tooltip("The operation type of the modifier.")] 
+        private ModifierOperation operation;
+        [SerializeField, Tooltip("The modifier amount.")] 
+        private float amount;
+        [SerializeField, Tooltip("If greater than -1, the time that the modifier will last.")] 
+        private float duration = -1;
         
         #region Properties /////////////////////////////////////////////////////////////////////////////////////////////
         
         /// <inheritdoc />
-        public string StatName { get => stat; }
+        public string StatName { get => statName; }
         
         /// <inheritdoc />
         public float Amount { get => amount; }
