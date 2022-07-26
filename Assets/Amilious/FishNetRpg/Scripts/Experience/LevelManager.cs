3869b8fa91d351246294cd574731fbf907c60656
Brandon Anderson <brandon.anderson@amilious.com>
2022-07-26 21:07:09 +0800

    0.0.1.Nightly.040
    - Added some test items.
    - Fixed the Level and Resource Manager.

--- Assets/Amilious/FishNetRpg/Scripts/Experience/LevelManager.cs
+++ Assets/Amilious/FishNetRpg/Scripts/Experience/LevelManager.cs
@@ -1,13 +1,59 @@
+using System;
 using Amilious.FishNetRpg.Entities;
 using Amilious.FishNetRpg.Modifiers;
 using FishNet.Object;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace Amilious.FishNetRpg.Experience {
     
     [RequireComponent(typeof(Entity),typeof(ModifierManager))]
     [AddComponentMenu(FishNetRpg.COMPONENT_MANAGERS+"Level Manager")]
-    public class LevelManager : NetworkBehaviour {
+    public class LevelManager : NetworkBehaviour, ISystemManager {
         
+        private Entity _entity;
+        
+        /// <inheritdoc />
+        public Systems System => Systems.ResourceSystem;
+
+        /// <inheritdoc />
+        public Type SystemType => GetType();
+        
+        /// <inheritdoc />
+        public Entity Entity {
+            get {
+                //get a reference to the entity if it does not exist.
+                _entity ??= GetComponent<Entity>();
+                return _entity; //return the entities reference.
+            }
+        }
+
+        public bool ApplyModifier(Object source, IModifier modifier) {
+            if(modifier.System != System) return false;
+            throw new NotImplementedException();
+        }
+
+        public bool ApplyModifier(int sourceId, IModifier modifier) {
+            if(modifier.System != System) return false;
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveModifier(Object source, IModifier modifier) {
+            if(modifier.System != System) return false;
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveModifier(int sourceId, IModifier modifier) {
+            if(modifier.System != System) return false;
+            throw new NotImplementedException();
+        }
+
+        public void RemoveModifiersFromSource(Object source) {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveModifiersFromSource(int sourceId) {
+            throw new NotImplementedException();
+        }
     }
 }
--- Assets/Amilious/FishNetRpg/Scripts/Resource/ResourceManager.cs
+++ Assets/Amilious/FishNetRpg/Scripts/Resource/ResourceManager.cs
@@ -1,13 +1,59 @@
+using System;
+using UnityEngine;
+using FishNet.Object;
+using Object = UnityEngine.Object;
 using Amilious.FishNetRpg.Entities;
 using Amilious.FishNetRpg.Modifiers;
-using FishNet.Object;
-using UnityEngine;
 
 namespace Amilious.FishNetRpg.Resource {
     
     [RequireComponent(typeof(Entity),typeof(ModifierManager))]
     [AddComponentMenu(FishNetRpg.COMPONENT_MANAGERS+"Resource Manager")]
-    public class ResourceManager : NetworkBehaviour {
+    public class ResourceManager : NetworkBehaviour, ISystemManager {
+
+        private Entity _entity;
         
+        /// <inheritdoc />
+        public Systems System => Systems.ResourceSystem;
+        
+        /// <inheritdoc />
+        public Type SystemType => GetType();
+        
+        /// <inheritdoc />
+        public Entity Entity {
+            get {
+                //get a reference to the entity if it does not exist.
+                _entity ??= GetComponent<Entity>();
+                return _entity; //return the entities reference.
+            }
+        }
+        
+        public bool ApplyModifier(Object source, IModifier modifier) {
+            if(modifier.System != System) return false;
+            throw new NotImplementedException();
+        }
+
+        public bool ApplyModifier(int sourceId, IModifier modifier) {
+            if(modifier.System != System) return false;
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveModifier(Object source, IModifier modifier) {
+            if(modifier.System != System) return false;
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveModifier(int sourceId, IModifier modifier) {
+            if(modifier.System != System) return false;
+            throw new NotImplementedException();
+        }
+
+        public void RemoveModifiersFromSource(Object source) {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveModifiersFromSource(int sourceId) {
+            throw new NotImplementedException();
+        }
     }
 }
