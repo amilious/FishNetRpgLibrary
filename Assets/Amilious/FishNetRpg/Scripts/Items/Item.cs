6ac30175175fbdec0a7eee7d52e82f6d728f89c2
Brandon Anderson <brandon.anderson@amilious.com>
2022-07-26 20:27:02 +0800

    0.0.1.Nightly.039
    - Updated the base item class.

--- Assets/Amilious/FishNetRpg/Scripts/Items/Item.cs
+++ Assets/Amilious/FishNetRpg/Scripts/Items/Item.cs
@@ -1,6 +1,11 @@
-using Amilious.Core;
-using Amilious.FishNetRpg.Pickups;
 using UnityEngine;
+using System.Linq;
+using Amilious.Core;
+using System.Collections.Generic;
+using Amilious.FishNetRpg.Pickups;
+using Amilious.FishNetRpg.Entities;
+using Amilious.FishNetRpg.Modifiers;
+using Amilious.FishNetRpg.Requirements;
 
 namespace Amilious.FishNetRpg.Items {
     
@@ -8,29 +13,119 @@ namespace Amilious.FishNetRpg.Items {
         menuName = FishNetRpg.INVENTORY_MENU_ROOT+"Standard Item", order = 0)]
     public class Item : AmiliousScriptableObject {
 
-        [SerializeField] private string displayName;
-        [SerializeField] private string description;
-        [SerializeField] private Sprite icon;
-        [SerializeField] private int maxStack = 1;
-        [SerializeField] private Pickup pickup;
+        #region Inspector Fields ///////////////////////////////////////////////////////////////////////////////////////
+        
+        [Header("Item Settings")]
+        [SerializeField, Tooltip("The display name for the item.")] 
+        private string displayName;
+        [SerializeField, Tooltip("The description for the item.")] 
+        private string description;
+        [SerializeField, Tooltip("The inventory icon for the item.")] 
+        private Sprite icon;
+        [SerializeField, Tooltip("The max stack size for the item.")] 
+        private int maxStack = 1;
+        [SerializeField, Tooltip("The pickup for the item.")] 
+        private Pickup pickup;
+        [SerializeField, Tooltip("Requirements for picking up the item.")]
+        private List<AbstractRequirement> pickupRequirements = new();
+        [SerializeField, Tooltip("Modifiers that are applied to an entity's when the item is in its inventory.")] 
+        private List<Modifier> inventoryModifiers = new();
 
-        private bool IsStackable => maxStack > 1;
-        private string DisplayName => displayName;
-        private string Description => description;
-        private Sprite Icon => icon;
-        private int MaxStackSize => Mathf.Max(1, maxStack);
+        #endregion /////////////////////////////////////////////////////////////////////////////////////////////////////
+        
+        #region Properties /////////////////////////////////////////////////////////////////////////////////////////////
+        
+        /// <summary>
+        /// This property is true if the item is stackable.
+        /// </summary>
+        public bool IsStackable => maxStack > 1;
+        
+        /// <summary>
+        /// This property contains the item's display name.
+        /// </summary>
+        public string DisplayName => displayName;
+        
+        /// <summary>
+        /// This property contains the item's description.
+        /// </summary>
+        public string Description => description;
+        
+        /// <summary>
+        /// This property contains the item's inventory icon.
+        /// </summary>
+        public Sprite Icon => icon;
+        
+        /// <summary>
+        /// This property contains the max stack size for this item.
+        /// </summary>
+        public int MaxStackSize => Mathf.Max(1, maxStack);
+        
+        #endregion /////////////////////////////////////////////////////////////////////////////////////////////////////
 
-        public Pickup SpawnPickup(Transform transform, int quantity) {
-            return SpawnPickup(transform.position, quantity, transform);
+        #region Modifier Methods ///////////////////////////////////////////////////////////////////////////////////////
+
+        /// <summary>
+        /// This method is used to apply the modifiers that are applied when the item is in an inventory.
+        /// </summary>
+        /// <param name="entity">The entity that the modifiers should be added to.</param>
+        public void ApplyInventoryModifiers(Entity entity) {
+            if(inventoryModifiers.Count == 0) return;
+            entity.ApplyModifiers(this,inventoryModifiers);
         }
 
-        public Pickup SpawnPickup(Vector3 position, int quantity, Transform parent = null) {
+        /// <summary>
+        /// This method is used to remove modifiers that are applied when the item is in an inventory.
+        /// </summary>
+        /// <param name="entity">The entity that the modifiers should be added to.</param>
+        public void RemoveInventoryModifiers(Entity entity) {
+            if(inventoryModifiers.Count == 0) return;
+            entity.RemoveModifiers(this,inventoryModifiers);
+        }
+        
+        #endregion /////////////////////////////////////////////////////////////////////////////////////////////////////
+        
+        #region Public Requirement Methods /////////////////////////////////////////////////////////////////////////////
+
+        /// <summary>
+        /// This method is used to check if the entity meets the requirements to pickup this item.
+        /// </summary>
+        /// <param name="entity">The entity that you want to check.</param>
+        /// <returns>True if the entity meets the requirements to pickup this item.</returns>
+        public bool MeetsPickupRequirements(Entity entity) {
+            return pickupRequirements.Count == 0 || 
+                   pickupRequirements.All(x => x.MeetsRequirement(entity));
+        }
+        
+        #endregion /////////////////////////////////////////////////////////////////////////////////////////////////////
+        
+        #region Pickup Spawning Methods ////////////////////////////////////////////////////////////////////////////////
+        
+        /// <summary>
+        /// This method is used to spawn a pickup for this item.
+        /// </summary>
+        /// <param name="transform">The transform for this item.</param>
+        /// <param name="quantity">The quantity of this item.</param>
+        /// <returns>The spawned pickup.</returns>
+        public Pickup SpawnPickup(Transform transform, int quantity) {
+            return SpawnPickup(transform.position, transform.rotation, quantity, transform);
+        }
+
+        /// <summary>
+        /// This method is used to spawn a pickup for this item.
+        /// </summary>
+        /// <param name="position">The position of the pickup.</param>
+        /// <param name="rotation">The rotation of the pickup.</param>
+        /// <param name="quantity">The item quantity.</param>
+        /// <param name="parent">A parent transform for the spawned pickup.</param>
+        /// <returns>The spawned pickup.</returns>
+        public Pickup SpawnPickup(Vector3 position, Quaternion rotation, int quantity, Transform parent = null) {
             if(pickup == null) return null;
-            var spawnedPickup = Instantiate(pickup,position,Quaternion.identity,parent);
+            var spawnedPickup = Instantiate(pickup,position,rotation,parent);
             spawnedPickup.Setup(this,quantity);
             return spawnedPickup;
         }
         
+        #endregion /////////////////////////////////////////////////////////////////////////////////////////////////////
+        
     }
-    
 }
